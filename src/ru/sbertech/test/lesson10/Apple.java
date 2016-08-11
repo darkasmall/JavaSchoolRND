6c128570269494f2cf41b5aeca19dad48314bbac
darkasmall <darkasmall@mail.ru>
2016-08-11 20:52:36 +0300

    Lesson 10

---
+++ src/ru/sbertech/test/lesson10/Apple.java
@@ -0,0 +1,18 @@
+package ru.sbertech.test.lesson10;
+
+/**
+ * Created by Student on 11.08.2016.
+ */
+public class Apple {
+    Integer size;
+
+    public Integer getSize() {
+        return size;
+    }
+
+    //public static Integer getSize(Integer size) { return size; }
+
+    public Apple(Integer size) {
+        this.size = size;
+    }
+}
---
+++ src/ru/sbertech/test/lesson10/LTest.java
@@ -0,0 +1,23 @@
+package ru.sbertech.test.lesson10;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+
+/**
+ * Created by Student on 11.08.2016.
+ */
+public class LTest {
+    public static void main(String[] args){
+        BiPredicate<Apple, Apple> p = (o1, o2) -> o1.getSize().equals(o2.getSize());
+        Comparator.comparing(Apple::getSize).reversed();
+
+
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+        int sum = list.stream().reduce(0, (a,b) -> a + b);
+        System.out.println(sum);
+
+    }
+}
